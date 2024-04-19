6171c4da948165848b8a84878ea8faed938afe96
Peter Kwesi Obrempong Stephenson <144380171+Obrempong12@users.noreply.github.com>
2024-04-19 17:26:44 +0000

    Update amenity.py

--- models/amenity.py
+++ models/amenity.py
@@ -1,10 +1,21 @@
+#!/usr/bin/python
+""" holds class Amenity"""
+import models
+from models.base_model import BaseModel, Base
+from os import getenv
+import sqlalchemy
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
-from models.base_model import BaseModel, Base
+
 
 class Amenity(BaseModel, Base):
-    __tablename__ = 'amenities'
+    """Representation of Amenity """
+    if models.storage_t == 'db':
+        __tablename__ = 'amenities'
+        name = Column(String(128), nullable=False)
+    else:
+        name = ""
 
-    name = Column(String(128), nullable=False)
-
-    places = relationship("Place", secondary='place_amenity', back_populates="amenities")
+    def __init__(self, *args, **kwargs):
+        """initializes Amenity"""
+        super().__init__(*args, **kwargs)
