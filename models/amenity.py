669ce4459eda5e92617b912ab9b4bce938991dd6
Peter Kwesi Obrempong Stephenson <144380171+Obrempong12@users.noreply.github.com>
2024-04-19 16:54:05 +0000

    Updates amenity.py

--- models/amenity.py
+++ models/amenity.py
@@ -1,15 +1,10 @@
-from models.base_model import BaseModel, Base
-from sqlalchemy import Column, String, Table, ForeignKey
+from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
+from models.base_model import BaseModel, Base
 
 class Amenity(BaseModel, Base):
     __tablename__ = 'amenities'
 
-    name = Column(String(128), nullable=False)
+    name = Column(String(128), nullable=False, unique=True)
 
-    place_amenities = relationship('Place', secondary='place_amenity')
-
-place_amenity = Table('place_amenity', Base.metadata,
-                      Column('place_id', String(60), ForeignKey('places.id'), primary_key=True),
-                      Column('amenity_id', String(60), ForeignKey('amenities.id'), primary_key=True)
-                      )
+    places = relationship("Place", secondary='place_amenity', back_populates="amenities")
